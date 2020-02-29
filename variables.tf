a61cc691b7afbf9e0e8f5a1b6b3ee94b2148c8a5
Eric Crews <eric@voxel51.com>
2020-02-29 10:17:53 -0500

    Use object type for node_pool variable

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
 variable "node_pool" {
-  type = map
+  type = object({
+    node_count = number
+    node_config = object({
+      preemptible  = bool
+      machine_type = string
+    })
+  })
 }
 
 variable "project" {
