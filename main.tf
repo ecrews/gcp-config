c08c26e4f58f0fb05e56ea3d38c466d60be0a0c6
Eric Crews <eric.crews@ithaka.org>
2020-05-05 10:06:39 -0400

    Change project + add two clusters

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "durable-footing-243118-tf-state"
+    bucket      = "playground-276313-tf-state"
     credentials = "service-account.json"
     prefix      = "terraform/state"
   }
@@ -13,8 +13,28 @@ provider "google" {
   version     = "~> 3.8"
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
+resource "google_container_cluster" "argocd" {
+  name     = "argocd"
+  location = var.region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_cluster" "flux" {
+  name     = "flux"
   location = var.region
 
   # We can't create a cluster with no node pool defined, but we want to only use
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,7 +3,7 @@ project = "durable-footing-243118"
 region = "us-central1"
 
 node_pool = {
-  node_count = 0
+  node_count = 1
   node_config = {
     preemptible  = true
     machine_type = "n1-standard-1"
