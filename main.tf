6d0e0585375a8011a8394c308107317c66e71b4c
Eric Crews <eric.crews@ithaka.org>
2020-05-05 10:10:21 -0400

    Update node pool name + add node pool for other cluster

--- main.tf
+++ main.tf
@@ -53,10 +53,31 @@ resource "google_container_cluster" "flux" {
   }
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
+resource "google_container_node_pool" "argocd_preemptible_nodes" {
   name       = "primary"
   location   = var.region
-  cluster    = google_container_cluster.primary.name
+  cluster    = google_container_cluster.argocd.name
+  node_count = var.node_pool.node_count
+
+  node_config {
+    preemptible  = var.node_pool.node_config.preemptible
+    machine_type = var.node_pool.node_config.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "flux_preemptible_nodes" {
+  name       = "primary"
+  location   = var.region
+  cluster    = google_container_cluster.flux.name
   node_count = var.node_pool.node_count
 
   node_config {
